ba393650e300a26252e79bc4f948c33b801e4f75
unknown <you@example.com>
2024-06-12 00:30:03 +0100

    mongodb-added

---
+++ backend/controller/auth.controller.js
@@ -0,0 +1,17 @@
+
+export const signup =  async(req, res) => {
+    try{
+        const {fullName, userName, password, confirmPassword, gender}= req.body;
+
+    } catch(error){
+
+    }
+}
+
+export const login =  (req, res) => {
+    console.log("login user")
+}
+
+export const logout =  (req, res) => {
+    console.log("logout user")
+}
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,21 @@
-const express = require("express");
-const dotenv = require("dotenv");
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
 
+dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-
+    // root route http://localhost:5000/
     res.send("Hello World!")
 })
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+app.use("/api/auth", authRoutes)
+
+app.listen(PORT, () => {
+    connectToMongoDB();
+    console.log(`Server is running on port ${PORT}`)
+})
